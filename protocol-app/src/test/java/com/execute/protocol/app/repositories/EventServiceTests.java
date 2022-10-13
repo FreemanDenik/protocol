d5f67c95307a81329157790470ed6afe84660bde
FreemanDenik <denikvy@gmail.com>
2022-10-13 13:42:20 +0300

    Переосмысленный тест EventTests

--- protocol-app/src/test/java/com/execute/protocol/app/repositories/EventServiceTests.java
+++ protocol-app/src/test/java/com/execute/protocol/app/repositories/EventServiceTests.java
@@ -6,7 +6,7 @@ import com.execute.protocol.core.services.EventService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 @DataJpaTest
-class EventTests {
+class EventServiceTests {
 
     @Autowired
     private EventRepository eventRepository;
@@ -34,22 +34,30 @@ class EventTests {
     void setEvents() {
         eventRepository.saveAll(
                 List.of(
-                        Event.builder().question("qwerty 1").updateTime(LocalDateTime.now().plusSeconds(1)).createTime(LocalDateTime.now()).build(),
-                        Event.builder().question("qwerty 2").updateTime(LocalDateTime.now().plusSeconds(2)).createTime(LocalDateTime.now()).build(),
-                        Event.builder().question("qwerty 3").updateTime(LocalDateTime.now().plusSeconds(3)).createTime(LocalDateTime.now()).build(),
-                        Event.builder().question("qwerty 4").updateTime(LocalDateTime.now().plusSeconds(4)).createTime(LocalDateTime.now()).build()
+                        Event.builder().question("question 1").updateTime(LocalDateTime.now().plusSeconds(1)).createTime(LocalDateTime.now()).build(),
+                        Event.builder().question("question 2").updateTime(LocalDateTime.now().plusSeconds(2)).createTime(LocalDateTime.now()).build(),
+                        Event.builder().question("question 3").updateTime(LocalDateTime.now().plusSeconds(3)).createTime(LocalDateTime.now()).build(),
+                        Event.builder().question("question 4").updateTime(LocalDateTime.now().plusSeconds(4)).createTime(LocalDateTime.now()).build(),
+                        Event.builder().question("question 5").updateTime(LocalDateTime.now().plusSeconds(5)).createTime(LocalDateTime.now()).build()
                 )
         );
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {4, 3, 2, 1})
-    void findAllByOrderByUpdateTimeDescTest(int pageSize) {
+    @CsvSource({
+        "1, question 5",
+        "2, question 5",
+        "3, question 5",
+        "4, question 5",
+        "5, question 5"
+    })
+   // @ValueSource(ints = {5, 4, 3, 2, 1}, strings = {"question 5"})
+    void findAllByOrderByUpdateTimeDescTest(int pageSize, String compare) {
         assertNotNull(eventService);
         Page<Event> page = eventService.getEventsOrderByUpdateTimeDesc(0, pageSize);
 
         List<Event> events = page.getContent();
-        assertEquals(events.get(0).getQuestion(), "qwerty 4");
+        assertEquals(events.get(0).getQuestion(), compare);
         assertEquals(events.size(), pageSize);
     }
 }
